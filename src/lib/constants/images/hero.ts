3a87d0135c146924eb8df742360ad20c5ca3f37f
Tina Huynh <tinamchuynh@gmail.com>
2025-05-25 21:53:53 -0700

    Add homepage hero images for enhanced visual appeal

---
+++ src/lib/constants/images/hero.ts
@@ -0,0 +1,5 @@
+export const homepageHeroImages = [
+  "https://images.unsplash.com/photo-1498503403619-e39e4ff390fe?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGx1eHVyeSUyMHRyYXZlbHxlbnwwfHwwfHx8MA%3D%3D",
+  "https://images.unsplash.com/photo-1483683804023-6ccdb62f86ef?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzN8fHRyYXZlbHxlbnwwfHwwfHx8MA%3D%3D",
+  "https://images.unsplash.com/photo-1516483638261-f4dbaf036963?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fHRyYXZlbHxlbnwwfHwwfHx8MA%3D%3D",
+];
