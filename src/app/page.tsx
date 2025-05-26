dada3810ca7dfda2a2dc6d8dbbcfb1b9dc8e784b
Tina Huynh <tinamchuynh@gmail.com>
2025-05-25 21:53:47 -0700

    Add layout and content to Example component for improved structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 "use client";
 
+
 export default function Example() {
-  return <div></div>;
+  return (
+    <div className="mx-auto pt-8 md:pt-12 lg:pt-24 w-10/12 md:w-11/12">
+      <h1>HI</h1>
+    </div>
+  );
 }
