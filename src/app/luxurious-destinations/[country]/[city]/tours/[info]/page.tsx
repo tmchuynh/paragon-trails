68d5f7fc3a5d080c8cbb10ebd8306ddf1520beb6
Tina Huynh <tinamchuynh@gmail.com>
2025-05-27 03:12:56 -0700

    Refactor TourPage component to include parameter destructuring and import necessary utilities for filtering tour guides

--- src/app/luxurious-destinations/[country]/[city]/tours/[info]/page.tsx
+++ src/app/luxurious-destinations/[country]/[city]/tours/[info]/page.tsx
@@ -1,4 +1,20 @@
-export default function TourPage() {
+"use client";
+
+import { tourGuides } from "@/lib/constants/staff/tourGuides";
+import { filterArrayByProperty } from "@/lib/utils/sort";
+
+export default function TourPage({
+  params,
+}: {
+  params: { country: string; city: string; info: string };
+}) {
+  // Directly destructure the params object
+  const { country, city, info } = params;
+
+  const value = info.replace(/-/g, " "); // Replace hyphens with spaces to match the property value
+  const filteredTourGuided = filterArrayByProperty(tourGuides, "city", city);
+
+  console.log("Filtered Tour Guides:", filteredTourGuided);
   return (
     <div className="flex flex-col justify-center items-center p-4 min-h-screen">
       <h1>Tour Information Details</h1>
