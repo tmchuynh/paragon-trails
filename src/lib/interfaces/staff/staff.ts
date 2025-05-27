c778d61fce7f26b2dc452f17a43985c670bca58f
Tina Huynh <tinamchuynh@gmail.com>
2025-05-27 03:13:02 -0700

    Remove country field from TourGuide interface for streamlined data structure

--- src/lib/interfaces/staff/staff.ts
+++ src/lib/interfaces/staff/staff.ts
@@ -21,7 +21,6 @@ export interface TourGuide extends TeamMember {
   city: string;
   state?: string;
   region?: string;
-  country: string;
   isPopular?: boolean;
   description: string;
   quote: string;
