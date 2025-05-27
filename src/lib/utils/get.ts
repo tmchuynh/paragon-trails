8805c65c25308c82b29487ca2e74f82be54bf1f2
Tina Huynh <tinamchuynh@gmail.com>
2025-05-26 17:57:34 -0700

    Adds utility functions for dynamic tool resource retrieval
    Implements a function to dynamically import tool resources based on toolkit and tool names, enhancing modularity and resource management.
    
    Introduces a new export for driver languages, consolidating language data from the driver qualification matrix for easier access and manipulation.

---
+++ src/lib/utils/get.ts
@@ -0,0 +1,31 @@
+import { driverQualificationMatrix } from "../constants/services/transportation/staff/drivers";
+
+export async function getToolResource(
+  toolKit: string,
+  tool: string,
+  toolKitID: string
+): Promise<any> {
+  try {
+    const toolModule = await import(
+      `@/lib/resources/toolkits/tools/${toolKit}/${tool}`
+    );
+    // Return the specific named export that matches toolKitID
+    if (toolModule[toolKitID]) {
+      return toolModule[toolKitID];
+    } else {
+      console.error(`Export named ${toolKitID} not found in module`);
+      return [];
+    }
+  } catch (error) {
+    console.error(`Error loading resource: ${error}`);
+    return [];
+  }
+}
+
+export const driverLanguages = Array.from(
+  new Set(
+    driverQualificationMatrix.flatMap((category) =>
+      category.drivers.flatMap((driver) => driver.languages)
+    )
+  )
+).sort();
