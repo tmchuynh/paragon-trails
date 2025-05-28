f1906a09ee76f5576310bcd2211cc39a45a87cd1
Tina Huynh <tinamchuynh@gmail.com>
2025-05-27 23:26:21 -0700

    Remove PackagePage component and associated layout

--- src/app/tour-packages/[package]/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function PackagePage() {
-  return (
-    <div className="flex flex-col justify-center items-center p-4 min-h-screen">
-      <header>
-        <h1>Tour Package Details</h1>
-      </header>
-    </div>
-  );
-}
