801ddab18df65c5fd63e5b5565e57503fcd3f222
Tina Huynh <tinamchuynh@gmail.com>
2025-05-27 22:25:18 -0700

    Add Tour and TourCategory interfaces to define tour attributes and structure

---
+++ src/lib/interfaces/services/tours.ts
@@ -0,0 +1,27 @@
+import { FAQs } from "@/lib/types/types";
+import { TourGuide } from "../people/staff";
+
+export interface TourCategory {
+  id: string;
+  title: string;
+  description: string;
+  tags?: string[];
+}
+
+export interface Tour {
+  title: string;
+  description: string;
+  images: string[];
+  duration: string;
+  price: string;
+  rating: number;
+  tags?: string[];
+  isFamilyFriendly?: boolean;
+  isHistorical?: boolean;
+  isRomantic?: boolean;
+  isOffTheBeatenPath?: boolean;
+  isLocalExperience?: boolean;
+  tourGuide: TourGuide["name"];
+  tourGuideId: TourCategory["id"];
+  faqs?: FAQs[];
+}
