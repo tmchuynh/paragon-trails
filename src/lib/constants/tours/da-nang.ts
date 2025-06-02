b9107409c83e2c20411f23c0b01453bf7bcb7a6a
Tina Huynh <tinamchuynh@gmail.com>
2025-06-01 18:08:15 -0700

    Update Da Nang tour details with new images and add new tours for the Fine Arts Museum and Night Market

--- src/lib/constants/tours/da-nang.ts
+++ src/lib/constants/tours/da-nang.ts
@@ -83,11 +83,11 @@ export const daNangTours: Tour[] = [
     description:
       "Visit the serene Linh Ung Pagoda, home to the tallest statue of the Lady Buddha in Vietnam, and enjoy breathtaking views of Da Nang from Son Tra Peninsula.",
     images: [
-      "https://media.istockphoto.com/id/1472794917/photo/ancient-temple-ruins-in-my-son-sanctuary-vietnam.jpg?s=612x612&w=0&k=20&c=obHd_BmpGScR4GRwgEn022QDCXwZ7Jw7GdXq3-w9vuQ=",
-      "https://media.istockphoto.com/id/1472794917/photo/ancient-temple-ruins-in-my-son-sanctuary-vietnam.jpg?s=612x612&w=0&k=20&c=obHd_BmpGScR4GRwgEn022QDCXwZ7Jw7GdXq3-w9vuQ=",
-      "https://media.istockphoto.com/id/1472794917/photo/ancient-temple-ruins-in-my-son-sanctuary-vietnam.jpg?s=612x612&w=0&k=20&c=obHd_BmpGScR4GRwgEn022QDCXwZ7Jw7GdXq3-w9vuQ=",
-      "https://media.istockphoto.com/id/1472794917/photo/ancient-temple-ruins-in-my-son-sanctuary-vietnam.jpg?s=612x612&w=0&k=20&c=obHd_BmpGScR4GRwgEn022QDCXwZ7Jw7GdXq3-w9vuQ=",
-      "https://media.istockphoto.com/id/1472794917/photo/ancient-temple-ruins-in-my-son-sanctuary-vietnam.jpg?s=612x612&w=0&k=20&c=obHd_BmpGScR4GRwgEn022QDCXwZ7Jw7GdXq3-w9vuQ=",
+      "https://media.istockphoto.com/id/1637036470/photo/aerial-view-drone-chua-linh-ung-bai-but-temple-lady-buddha-temple-in-da-nang-vietnam.jpg?s=612x612&w=0&k=20&c=iM4_0vgBvr5vQ6YAP9xOdPmbrfQ0j_wbpf0KJ6aLfF0=",
+      "https://media.istockphoto.com/id/2149465806/photo/linh-ung-pagoda-temple-translation-from-chinese-character-in-da-nang-city-vietnam.jpg?s=612x612&w=0&k=20&c=nTBVFCSWfi0F_7H-4kjvUI3O-ZzjD8Rv3mML_PJVyLg=",
+      "https://media.istockphoto.com/id/1201242261/photo/the-bronze-guanyin-statue-inside-tsz-shan-buddhist-monastery-in-tai-po-hong-kong.jpg?s=612x612&w=0&k=20&c=d4cU28Dnyfc0lMrsaReShRsiTk-bcJZOGuICuvzSdAE=",
+      "https://media.istockphoto.com/id/2168523613/photo/linh-ung-pagoda-on-son-tra-peninsula-a-famous-tourist-attraction-in-vietnam.jpg?s=612x612&w=0&k=20&c=2HABxlWBM8YRxGPjNaAFKvXsk8ReS_PJWspqDGPzVj0=",
+      "https://media.istockphoto.com/id/2187393387/photo/the-most-beautiful-viewpoint-lady-buddha-linh-ung-son-tra-in-da-nang-vietnam.jpg?s=612x612&w=0&k=20&c=mDcUS4Lgocqo0e-rdwoNgrOnqtI-DMhGwWkg8SlHLk4=",
     ],
     duration: "4 hours",
     price: "$50",
@@ -291,6 +291,380 @@ export const daNangTours: Tour[] = [
       "Meals",
       "Personal expenses",
       "Tips for the guide",
+      "Travel insurance",
+    ],
+  },
+  {
+    title: "Danang Fine Arts Museum",
+    description:
+      "Explore the Danang Fine Arts Museum, showcasing contemporary Vietnamese art and traditional crafts in a modern architectural setting.",
+    images: [
+      "https://lh3.googleusercontent.com/gps-cs-s/AC9h4nrgJY1UT_Cj-JsgMSyMs5fksfBTw9yMi_D52ICOtwgdnELP1QFjNjJ8HdG2L_x8tB0rc-DGj11O0N1caMwxBq-G0UjfGT0jhPe1uWRpMEb8BFGWCHblXJ-ifEn94G1_ao2nYJCAZg=w243-h304-n-k-no-nu",
+      "https://lh3.googleusercontent.com/gpms-cs-s/AB8u6HYKZmY8GjIBak3gZ2ffZU55hEYM6MdexZtQRkIkrTm6ler9RZQA7cWsAt3ZVWAdsE-qzKnkXAjQ3m-WJj9qTl_DA7uZt4hunnTFExwcDvY3jkEV0HMSYlO1zPScDNX8Yq0lDDa0pg=s294-w294-h220-n-k-no",
+      "https://lh3.googleusercontent.com/gps-cs-s/AC9h4nrGJaA7YbGGcfAG0xc8KtmbAfsUQRSaH9cbll5mp_RrH3eyVIn9gCzS5Hkfue37CGDuHKM1rBuY5G1vvl7tj7njo3AGPV7eb2TTLn205BIP3jngEjOCuLUANJVGxKvHCuWKZnpr4A=w243-h174-n-k-no-nu",
+      "https://lh3.googleusercontent.com/gps-cs-s/AC9h4npEaQE9aeDyqOWVOpYyxmq3Cl1U9f2nI1hdJx2rmAPppSXCAqemirCczX878IqvIHmj0S-yh7l6XERWGoIatZ0wzeQmK1SpGc9M-L7F29-ZwYEKZ84SMht3p0zI3bR8gBjQthk=w243-h174-n-k-no-nu",
+      "https://lh3.googleusercontent.com/gps-cs-s/AC9h4npcmHXyKBzKh4_O483mnV7RQZ7ytT2zRpt9u4Ykd34g2m8RilfGW0HxoH1PwpmIKumanOFufbamJcQVFgx4vKYJmr2Cci3zeVpscV0mWsC5U86yLMlgc-LSRnOyhvOlrBMwOPaH=w243-h174-n-k-no-nu",
+    ],
+    duration: "2 hours",
+    price: "$20",
+    rating: 4.4,
+    tags: ["Art", "Culture", "Photography"],
+    isHistorical: false,
+    isRomantic: false,
+    isOffTheBeatenPath: false,
+    isLocalExperience: true,
+    tourCategoryId: "art-and-music",
+    isAdventure: false,
+    isCultural: true,
+    isCulinary: false,
+    isLuxury: false,
+    isArtOrMusic: true,
+    isWellness: false,
+    isNightlife: false,
+    isSpiritual: false,
+    isPopular: true,
+    faqs: [
+      {
+        question: "Is there an entrance fee for the museum?",
+        answer: "Yes, the entrance fee is $20 per person.",
+      },
+      {
+        question: "What are the museum's opening hours?",
+        answer: "The museum is open daily from 9 AM to 6 PM.",
+      },
+      {
+        question: "Are guided tours available?",
+        answer:
+          "Yes, guided tours can be arranged in advance for an additional fee.",
+      },
+      {
+        question: "Is photography allowed inside the museum?",
+        answer: "Yes, photography is allowed, but please do not use flash.",
+      },
+      {
+        question: "Are there any special exhibitions currently on display?",
+        answer:
+          "Yes, the museum features rotating exhibitions of contemporary Vietnamese artists.",
+      },
+      {
+        question: "Is the museum accessible for visitors with mobility issues?",
+        answer:
+          "Yes, the museum is wheelchair accessible with ramps and elevators.",
+      },
+    ],
+    availableDates: [
+      "2024-02-05",
+      "2024-03-05",
+      "2024-03-15",
+      "2024-03-20",
+      "2024-03-30",
+      "2024-04-30",
+      "2024-05-05",
+      "2024-05-10",
+      "2024-05-15",
+      "2024-06-30",
+      "2024-07-25",
+      "2024-08-01",
+      "2024-08-05",
+      "2024-08-15",
+      "2024-09-30",
+      "2024-10-10",
+      "2024-10-15",
+      "2024-10-25",
+      "2024-10-30",
+    ],
+    meetingPoint: {
+      address: "Danang Fine Arts Museum, Da Nang",
+      coordinates: {
+        latitude: 16.0544,
+        longitude: 108.2461,
+      },
+      instructions: "Meet at the main entrance of the museum.",
+      contactNumber: "+84 123 456 789",
+    },
+    cancellationPolicy: "Free cancellation up to 24 hours before the tour.",
+    highlights: [
+      "Explore contemporary Vietnamese art",
+      "Discover traditional crafts and techniques",
+      "Learn about the history of Vietnamese art",
+      "Enjoy the modern architectural design of the museum",
+      "Capture stunning photographs of the exhibits",
+    ],
+    inclusions: [
+      "Local guide",
+      "Transportation to and from the museum",
+      "Bottled water",
+      "Entrance fees",
+      "Photography tips and guidance",
+    ],
+    exclusions: [
+      "Meals",
+      "Personal expenses",
+      "Tips for the guide",
+      "Travel insurance",
+    ],
+  },
+  {
+    title: "Danang Night Market Tour",
+    description:
+      "Experience the vibrant atmosphere of Danang's night market, sampling local street food, shopping for souvenirs, and enjoying live entertainment.",
+    images: [
+      "https://images.unsplash.com/photo-1576510683662-0f5a01be2114?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZGElMjBuYW5nJTIwbmlnaHQlMjBtYXJrZXR8ZW58MHx8MHx8fDA%3D",
+      "https://plus.unsplash.com/premium_photo-1673240845266-2f2c432cf194?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8ZGElMjBuYW5nJTIwbmlnaHQlMjBtYXJrZXR8ZW58MHx8MHx8fDA%3D",
+      "https://images.unsplash.com/photo-1723288305906-507b33690f0e?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8ZGElMjBuYW5nJTIwbmlnaHQlMjBtYXJrZXR8ZW58MHx8MHx8fDA%3D",
+      "https://images.unsplash.com/photo-1619546941564-be762f782981?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGRhJTIwbmFuZyUyMG5pZ2h0JTIwbWFya2V0fGVufDB8fDB8fHww",
+      "https://images.unsplash.com/photo-1590437084089-9f5ae1500176?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjV8fGRhJTIwbmFuZyUyMG5pZ2h0JTIwbWFya2V0fGVufDB8fDB8fHww",
+    ],
+    duration: "3 hours",
+    price: "$30",
+    rating: 4.3,
+    tags: ["Food", "Culture", "Shopping", "Entertainment"],
+    isHistorical: false,
+    isRomantic: false,
+    isOffTheBeatenPath: false,
+    isLocalExperience: true,
+    tourCategoryId: "culinary",
+    isAdventure: false,
+    isCultural: true,
+    isCulinary: true,
+    isLuxury: false,
+    isArtOrMusic: false,
+    isWellness: false,
+    isNightlife: true,
+    isSpiritual: false,
+    isPopular: true,
+    faqs: [
+      {
+        question: "What types of food are available at the night market?",
+        answer:
+          "The night market offers a wide variety of local street food, including seafood, grilled meats, fresh fruits, and traditional Vietnamese dishes.",
+      },
+      {
+        question: "Is the night market safe for tourists?",
+        answer:
+          "Yes, the night market is generally safe for tourists. However, it's always advisable to keep an eye on your belongings.",
+      },
+      {
+        question: "Are there any specific items to look for when shopping?",
+        answer:
+          "You can find unique handicrafts, souvenirs, clothing, and local snacks at the night market.",
+      },
+      {
+        question: "Is there live entertainment at the night market?",
+        answer:
+          "Yes, the night market often features live music, cultural performances, and street artists, creating a lively atmosphere.",
+      },
+      {
+        question: "What is the best time to visit the night market?",
+        answer:
+          "The night market is open from 5 PM to midnight, with the best time to visit being around 7 PM when it is most lively.",
+      },
+      {
+        question: "Is there an entrance fee for the night market?",
+        answer: "No, there is no entrance fee to visit the night market.",
+      },
+      {
+        question: "Are guided tours available for the night market?",
+        answer:
+          "Yes, guided tours can be arranged that include a local guide to help you navigate the market and recommend the best food stalls.",
+      },
+      {
+        question: "Can I pay with credit cards at the night market?",
+        answer:
+          "While some vendors may accept credit cards, it's best to carry cash in Vietnamese Dong for convenience, as many stalls only accept cash.",
+      },
+      {
+        question:
+          "Is the night market accessible for visitors with mobility issues?",
+        answer:
+          "Yes, the night market is generally accessible, but it can get crowded. It's advisable to go early to avoid large crowds.",
+      },
+      {
+        question:
+          "Are there any specific cultural etiquette tips for visiting the night market?",
+        answer:
+          "It's polite to greet vendors with a smile and 'Xin chào' (Hello). Bargaining is common, so feel free to negotiate prices respectfully.",
+      },
+    ],
+    availableDates: [
+      "2024-02-05",
+      "2024-03-05",
+      "2024-03-15",
+      "2024-03-20",
+      "2024-03-30",
+      "2024-04-30",
+      "2024-05-05",
+      "2024-05-10",
+      "2024-05-15",
+      "2024-06-30",
+      "2024-07-25",
+      "2024-08-01",
+      "2024-08-05",
+      "2024-08-15",
+      "2024-09-30",
+      "2024-10-10",
+      "2024-10-15",
+      "2024-10-25",
+      "2024-10-30",
+    ],
+    meetingPoint: {
+      address: "Danang Night Market, Da Nang",
+      coordinates: {
+        latitude: 16.0544,
+        longitude: 108.2461,
+      },
+      instructions: "Meet at the main entrance of the night market.",
+      contactNumber: "+84 123 456 789",
+    },
+    cancellationPolicy: "Free cancellation up to 24 hours before the tour.",
+    highlights: [
+      "Sample a variety of local street food",
+      "Shop for unique souvenirs and handicrafts",
+      "Enjoy live entertainment and cultural performances",
+      "Experience the vibrant atmosphere of Danang's night market",
+      "Learn about local customs and traditions from your guide",
+    ],
+    inclusions: [
+      "Local guide",
+      "Transportation to and from the night market",
+      "Bottled water",
+      "Entrance fees",
+      "Food samples from selected stalls",
+    ],
+    exclusions: [
+      "Additional food and drinks",
+      "Personal expenses",
+      "Tips for the guide",
+      "Travel insurance",
+    ],
+  },
+  {
+    title: "Club One Opera",
+    description:
+      "Experience the vibrant nightlife of Da Nang at Club One Opera, known for its electrifying atmosphere, live DJ performances, and stunning light shows.",
+    images: [
+      "https://lh3.googleusercontent.com/p/AF1QipP7fp4VNz3hezxfQ_NPMqDF30DEEEOWZSJQAzWU=w243-h174-n-k-no-nu",
+      "https://lh3.googleusercontent.com/gps-cs-s/AC9h4npUGJtLX0yMMjrVAuTQyDaBbkHKkY1vuxjVqCtRzh2NdQrzIvbmimmr6WHz0FfW7BN-TFOI-3ISSyrWsMxU0_4O9QNWfzr6lzMNuP7iGp75wuypas2ziwFXvAmY6G5RNWcO-I18=w243-h243-n-k-no-nu",
+      "https://lh3.googleusercontent.com/p/AF1QipOCIrlPYiHub6VwNi9phdj9Sgjx8ftuwfgdFISu=w243-h174-n-k-no-nu",
+      "https://lh3.googleusercontent.com/p/AF1QipPAEaDTm_o9LOTUvXlA4U7f7_NA2n35y7V3nUtx=w243-h174-n-k-no-nu",
+      "https://lh3.googleusercontent.com/p/AF1QipM4QviKmla6b2ikjV68M1i-GHlw-UvgQtdX-pAl=w243-h174-n-k-no-nu",
+    ],
+    duration: "4 hours",
+    price: "$150",
+    rating: 4.9,
+    tags: ["Nightlife", "Entertainment", "Music", "Dance", "Casino", "Luxury"],
+    isHistorical: false,
+    isRomantic: false,
+    isOffTheBeatenPath: false,
+    isLocalExperience: false,
+    tourCategoryId: "nightlife",
+    isAdventure: false,
+    isCultural: false,
+    isCulinary: false,
+    isLuxury: true,
+    isArtOrMusic: true,
+    isWellness: false,
+    isSpiritual: false,
+    isPopular: true,
+    faqs: [
+      {
+        question: "What is the dress code for Club One Opera?",
+        answer:
+          "The dress code is smart casual. No flip-flops or beachwear are allowed.",
+      },
+      {
+        question: "Is there an age restriction for entry?",
+        answer:
+          "Yes, guests must be 18 years or older to enter Club One Opera.",
+      },
+      {
+        question: "Are reservations required?",
+        answer:
+          "Reservations are recommended, especially for VIP tables, but walk-ins are welcome based on availability.",
+      },
+      {
+        question: "What types of music are played at the club?",
+        answer:
+          "Club One Opera features a mix of electronic dance music, hip-hop, and live performances by local and international DJs.",
+      },
+      {
+        question: "Is there a cover charge?",
+        answer:
+          "Yes, there is a cover charge of $20 per person, which includes one complimentary drink.",
+      },
+      {
+        question: "Are food and drinks available?",
+        answer:
+          "Yes, the club offers a variety of drinks and a limited menu of light snacks. Full dining options are available at the adjacent restaurant.",
+      },
+      {
+        question: "Is there a VIP area?",
+        answer:
+          "Yes, Club One Opera has a VIP area with exclusive seating and bottle service. Reservations are required for VIP tables.",
+      },
+      {
+        question: "What are the club's opening hours?",
+        answer: "Club One Opera is open from 8 PM to 2 AM daily.",
+      },
+      {
+        question: "Is there parking available?",
+        answer:
+          "Yes, there is a parking lot available for guests. Valet service is also offered for convenience.",
+      },
+      {
+        question: "Can I book a private event at Club One Opera?",
+        answer:
+          "Yes, Club One Opera offers private event bookings for parties, corporate events, and special occasions. Please contact the club for more details.",
+      },
+    ],
+    availableDates: [
+      "2024-02-05",
+      "2024-03-05",
+      "2024-03-15",
+      "2024-03-20",
+      "2024-03-30",
+      "2024-04-30",
+      "2024-05-05",
+      "2024-05-10",
+      "2024-05-15",
+      "2024-06-30",
+      "2024-07-25",
+      "2024-08-01",
+      "2024-08-05",
+      "2024-08-15",
+      "2024-09-30",
+      "2024-10-10",
+      "2024-10-15",
+      "2024-10-25",
+      "2024-10-30",
+    ],
+    meetingPoint: {
+      address: "Club One Opera, Da Nang",
+      coordinates: {
+        latitude: 16.0544,
+        longitude: 108.2461,
+      },
+      instructions: "Meet at the main entrance of Club One Opera.",
+      contactNumber: "+84 123 456 789",
+    },
+    cancellationPolicy: "Free cancellation up to 24 hours before the tour.",
+    highlights: [
+      "Experience the electrifying nightlife of Da Nang",
+      "Enjoy live DJ performances and stunning light shows",
+      "Dance the night away in a luxurious setting",
+      "Indulge in premium drinks and VIP service",
+      "Capture unforgettable moments in a vibrant atmosphere",
+    ],
+    inclusions: [
+      "VIP table reservation (optional)",
+      "Complimentary drink",
+      "Access to the club's facilities",
+      "Photography opportunities",
+    ],
+    exclusions: [
+      "Additional food and drinks",
+      "Personal expenses",
+      "Tips for the staff",
       "Travel insurance",
     ],
   },
