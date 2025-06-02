8412b152275852ea05d637ded9d2495b2d0ace12
Tina Huynh <tinamchuynh@gmail.com>
2025-06-02 08:39:55 -0700

    Refactor BookYourTripToday component: Remove unused imports to streamline the code and improve readability.

--- src/app/book-your-trip-today/page.tsx
+++ src/app/book-your-trip-today/page.tsx
@@ -1,29 +1,5 @@
 "use client";
-import TourCard from "@/components/cards/TourCard";
-import Loading from "@/components/Loading";
-import { Button } from "@/components/ui/button";
-import { Calendar } from "@/components/ui/calendar";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
-import { cityattractions } from "@/lib/constants/info/city";
-import { Tour } from "@/lib/interfaces/services/tours";
-import { cn } from "@/lib/utils";
-import { displayRatingStars } from "@/lib/utils/displayRatingStars";
-import { capitalize, formatToSlug } from "@/lib/utils/format";
-import {
-  findGuideBySpecialty,
-  getAllTours,
-  getTourData,
-  getToursByCategory,
-} from "@/lib/utils/get";
-import { format } from "date-fns";
-import { CalendarIcon } from "lucide-react";
-import Image from "next/image";
-import { useRouter, useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function BookYourTripToday() {
   const router = useRouter();
