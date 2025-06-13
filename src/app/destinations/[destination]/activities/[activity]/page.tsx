7833bd92e5f243e5b66ebf17bedba6f0c7cc683d
Tina Huynh <tinamchuynh@gmail.com>
2025-06-13 15:26:49 -0700

    Adds Activity Details page component
    Introduces a new component for displaying details about specific activities. This enhancement improves user experience by providing a dedicated section for activity information.

---
+++ src/app/destinations/[destination]/activities/[activity]/page.tsx
@@ -0,0 +1,8 @@
+export default function ActivityDetailsPage() {
+  return (
+    <div>
+      <h1>Activity Details</h1>
+      <p>Details about a specific activity will be displayed here.</p>
+    </div>
+  );
+}
