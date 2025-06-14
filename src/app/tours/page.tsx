b3fa12c0ab0edf1ebd519abbd53734f9c40bfdde
Tina Huynh <tinamchuynh@gmail.com>
2025-06-14 00:56:29 -0700

    Updates tour selection options in the UI
    Replaces existing selection items with updated options to improve user experience.
    
    Changes the selectable values from 6, 9, 18 to 4, 8, 16, and adds 24, providing a more relevant range for users.
    
    Improves the clarity and utility of the tour selection feature.

--- src/app/tours/page.tsx
+++ src/app/tours/page.tsx
@@ -428,10 +428,11 @@ export default function FindToursPage() {
                       <SelectValue />
                     </SelectTrigger>
                     <SelectContent className="w-full max-h-60">
-                      <SelectItem value="6">6</SelectItem>
-                      <SelectItem value="9">9</SelectItem>
+                      <SelectItem value="4">4</SelectItem>
+                      <SelectItem value="8">8</SelectItem>
                       <SelectItem value="12">12</SelectItem>
-                      <SelectItem value="18">18</SelectItem>
+                      <SelectItem value="16">16</SelectItem>
+                      <SelectItem value="24">24</SelectItem>
                     </SelectContent>
                   </Select>
                 </div>
