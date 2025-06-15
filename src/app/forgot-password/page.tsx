ad234d7755f327d15b1ecb4480774559f6932e59
Tina Huynh <tinamchuynh@gmail.com>
2025-06-14 18:27:38 -0700

    Remove Forgot Password page component

--- src/app/forgot-password/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function ForgotPasswordPage() {
-  return (
-    <div>
-      <h1>Forgot Password</h1>
-      <p>Please enter your email to reset your password.</p>
-      {/* Add form for email input and submit button here */}
-    </div>
-  );
-}
